6a0b10b54d103038915f1b06fac75cf5a890bb97
user <user@DESKTOP-COJ9858>
2023-11-03 13:54:56 +0900

    6번까지

---
+++ zzJavaStudy/src/javaJungseok/ch04/Exercisse4_6.java
@@ -0,0 +1,16 @@
+package javaJungseok.ch04;
+
+public class Exercisse4_6 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=6;i++) {
+			for(int j=1; j<=6;j++) {
+				int sum = i+j;
+				if(i+j==6) {
+					System.out.println(i+"+"+j+"="+sum);
+				}
+			}
+		}
+		
+	} // end of main
+} // end of class
